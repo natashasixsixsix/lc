463503a7a1f541901861d63e4ff78ac2dfb28f8f
natashasixsixsix <natasha_work_0887@163.com>
2022-11-30 10:38:14 +0800

    2022-11-30 10:37:22 -[108]将有序数组转换为二叉搜索树.java -[110]平衡二叉树.java：java基本数据类型传递与引用传递区别

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        boolean bol = true;
+        dfs(bol);
+        System.out.println(bol);
+    }
+    public static void dfs(boolean bol){
+        bol = false;
     }
 }
